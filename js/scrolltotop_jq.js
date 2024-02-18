cf02e25867acde8242d1a52f2981494f42ff81f9
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2024-02-18 13:52:10 +0200

    new updates.

---
+++ js/scrolltotop_jq.js
@@ -0,0 +1,41 @@
+
+(function ($) {
+	$.fn.SLScrollToTop	= function(options) {
+		var defaults	= {
+			'id':			'scrollToTop',
+			'className':	'scrollToTop',
+			'image':		'',
+			'text':			'^ Scroll to Top',
+			'title':		'Scroll to Top',
+			'duration':		500
+		};
+		
+		var options		= $.extend(defaults, options);
+		var $link		= $('<div />', {
+			'id':		options.id,
+			'html':		options.text,
+			'title':	options.title,
+			'class':	options.className
+		}).appendTo('body').hide();
+
+		if (options.image != '') {
+			$('<img src="' + options.image + '" alt="' + options.title + '" />').prependTo($link);
+		}
+
+		$(window).scroll(function() {
+			$this	= $(this);
+
+			if ($this.scrollTop() > 150) {
+				$link.fadeIn();
+			} else {
+				$link.fadeOut();
+			}
+		});
+
+		$link.click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, options.duration);
+		});
+	}
+})(jQuery);
